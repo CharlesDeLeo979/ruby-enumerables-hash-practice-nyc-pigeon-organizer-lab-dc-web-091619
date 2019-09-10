4eb6e2e58176f841655ad14482018b8738995491
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-10 15:41:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require "pry"
 def nyc_pigeon_organizer(pigeon_data)
   new_hash = {}
@@ -19,4 +20,44 @@ def nyc_pigeon_organizer(pigeon_data)
     end
   end
   return new_hash
+=======
+def nyc_pigeon_organizer(data)
+  pigeon_list = {
+    "Theo" => {
+      :color => ["purple", "grey"],
+      :gender => ["male"]
+      :lives => ["Subway"]
+    }, 
+    "Peter Jr." => {
+      :color => ["purple", "grey"],
+      :gender => ["male"]
+      :lives => ["Library"]
+    }, 
+    "Lucky" => {
+     :color => ["purple"],
+     :gender => ["male"], 
+     :lives => ["Central Park"]
+    }, 
+    "Ms. K" => {
+      :color => ["grey", "white"], 
+      :gender => ["female"], 
+      :lives => ["Central Park"]
+    },
+    "Queenie" => {
+      :color => ["white", "brown"], 
+      :gender => ["female"], 
+      :lives => ["Subway"]
+    },
+    "Andrew" => {
+      :color => ["white"], 
+      :gender => ["male"], 
+      :lives => ["City Hall"]
+    }, 
+    "Alex" => {
+      :color => ["white", "brown"], 
+      :gender => ["male"], 
+      :lives => ["Central Park"]
+    }
+  }
+>>>>>>> 0f60903d86be52ac6ed25ecfee6cc095c7fc9db3
 end
